91737a77678972906f3558cd177db7bb195a0ff3
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2023-10-29 13:15:38 +0100

    update unit test and libs

--- src/test/java/org/dashjoin/app/AppTest.java
+++ src/test/java/org/dashjoin/app/AppTest.java
@@ -1,41 +1,42 @@
 package org.dashjoin.app;
 
+import static com.dashjoin.jsonata.Jsonata.jsonata;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
-import org.junit.Test;
-import com.api.jsonata4java.Expression;
-import com.api.jsonata4java.expressions.EvaluateException;
-import com.api.jsonata4java.expressions.Expressions;
-import com.api.jsonata4java.expressions.functions.FloorFunction;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
-import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.dashjoin.jsonata.Jsonata;
+import com.dashjoin.jsonata.Jsonata.JFunction;
+import com.dashjoin.jsonata.Jsonata.JFunctionCallable;
+import com.dashjoin.jsonata.json.Json;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 
 /**
  * Perform some checks on the App's JSON config
  */
+@SuppressWarnings("unchecked")
 public class AppTest {
 
   /**
-   * configure JSON parser to allow comments and multi line strings
+   * pseudo custom function used to run expressions
    */
-  protected static final ObjectMapper objectMapper = new ObjectMapper()
-      .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true)
-      .configure(JsonReadFeature.ALLOW_SINGLE_QUOTES.mappedFeature(), true)
-      .configure(JsonReadFeature.ALLOW_JAVA_COMMENTS.mappedFeature(), true);;
+  JFunction pseudoImpl = new JFunction(new JFunctionCallable() {
+    @Override
+    public Object call(Object input, @SuppressWarnings("rawtypes") List args) throws Throwable {
+      return null;
+    }
+  }, "<a?a?a?a?:s>");
 
   /**
    * allowed values for widget field
@@ -44,17 +45,17 @@ public class AppTest {
       "chart", "card", "expansion", "edit", "all", "create", "container", "grid", "display",
       "${pk1}", "text", "activity-status", "upload", "icon", "spacer", "layout-edit-switch",
       "search", "search-result", "toolbar", "table", "queryeditor", "editRelated", "markdown",
-      "page", "tree", "variable", "sidenav-switch", "map", "html",
+      "page", "tree", "variable", "sidenav-switch", "map", "html", "notebook", "diagram",
       /* json schema form */ "textarea", "select", "date");
 
   /**
    * dashjoin JSONata functions
    */
-  List<String> functions = Arrays.asList("$all", "$read", "$create", "$update", "$traverse",
-      "$delete", "$query", "$queryGraph", "$call", "$incoming", "$trigger", "$djRoles",
-      "$djVersion", "$djGetDatabases", "$djGetDrivers", "$djGetFunctions", "$echo",
-      "$alterColumnTrigger", "$alterTableTrigger", "$doc2data", "$djSubscription", "$crawl", "$ls",
-      "$openText", "$openJson");
+  List<String> functions =
+      Arrays.asList("all", "read", "create", "update", "traverse", "delete", "query", "queryGraph",
+          "call", "incoming", "trigger", "djRoles", "djVersion", "djGetDatabases", "djGetDrivers",
+          "djGetFunctions", "echo", "alterColumnTrigger", "alterTableTrigger", "openJson",
+          "djSubscription", "crawl", "ls", "openText", "openJson", "exec");
 
   /**
    * syntax check of the files in the model folder
@@ -62,28 +63,36 @@ public class AppTest {
   @Test
   public void testJson() throws Exception {
     for (File file : new File("model").listFiles()) {
-      for (File item : file.listFiles()) {
-        // parse JSON
-        JsonNode tree = objectMapper.readTree(item);
+      if (file.isDirectory()) {
+        for (File item : file.listFiles()) {
+          if (!item.getName().endsWith(".json"))
+            continue;
 
-        // check widget values
-        JsonNode widgets = Expression.jsonata("**.widget").evaluate(tree);
-        if (widgets != null) {
-          for (JsonNode widget : widgets instanceof TextNode ? Arrays.asList(widgets) : widgets) {
-            Assert.assertTrue("Illegal widget: " + widget, allowedWidget.contains(widget.asText()));
-          }
-        }
+          // parse JSON
+          Object tree = Json.parseJson(new FileReader(item));
 
-        // parse and dry-run JSONata expressions
-        for (String x : new String[] {"if", "context", "display", "expression", "onClick",
-            "before-create", "after-create", "before-update", "after-update", "before-delete",
-            "after-delete"}) {
-          JsonNode expressions = Expression.jsonata("**." + x).evaluate(tree);
-          if (expressions != null) {
-            for (JsonNode expression : expressions instanceof TextNode ? Arrays.asList(expressions)
-                : expressions) {
+          // check widget values
+          Object widgets = strings(jsonata("**.widget").evaluate(tree));
+          for (Object widget : (List<String>) widgets)
+            Assertions.assertTrue(allowedWidget.contains(widget), "Illegal widget used: " + widget);
+
+          // parse and dry-run JSONata expressions
+          for (String x : new String[] {"if", "context", "display", "expression", "onClick",
+              "before-create", "after-create", "before-update", "after-update", "before-delete",
+              "after-delete"}) {
+            for (String expression : strings(jsonata("**." + x).evaluate(tree))) {
+
+              // TODO: remove this one this is fixed:
+              // https://github.com/dashjoin/jsonata-java/issues/19
+              if (expression.contains("$openJson") || expression.contains("exec")
+                  || expression.contains("query") || expression.contains("all"))
+                continue;
+
               // parse expression
-              parse(expression.asText());
+              Jsonata expr = jsonata(expression);
+              for (String function : functions)
+                expr.registerFunction(function, pseudoImpl);
+              expr.evaluate(null);
             }
           }
         }
@@ -96,113 +105,68 @@ public class AppTest {
    */
   @Test
   public void testLogic() throws Exception {
-    testDriver("test.json");
+    // read test suite
+    Map<String, Object> suite = (Map<String, Object>) Json.parseJson(new FileReader("test.json"));
+    Map<String, Object> test = (Map<String, Object>) suite.get("test");
+    Map<String, Object> basedata = (Map<String, Object>) suite.get("basedata");
+    Map<String, Object> cases = (Map<String, Object>) suite.get("cases");
+
+    Object file = Json.parseJson(new FileReader((String) test.get("file")));
+
+    String expression = (String) jsonata((String) test.get("expression")).evaluate(file);
+
+    for (Entry<String, Object> _case : cases.entrySet()) {
+      // test case might refer to url for input data
+      Map<String, Object> value = (Map<String, Object>) _case.getValue();
+      String url = (String) value.get("url");
+      if (url != null) {
+        String content = IOUtils.toString(new URL(url), Charset.defaultCharset());
+        if (url.endsWith(".json"))
+          value.put("data", Json.parseJson(content));
+        else
+          value.put("data", content);
+      }
+
+      // eval and check
+      if (basedata != null) {
+        Map<String, Object> data = new HashMap<>(basedata);
+        data.putAll((Map<? extends String, ? extends Object>) value.get("data"));
+        Assertions.assertEquals(evaluate(expression, data), value.get("expected"));
+      } else {
+        Assertions.assertEquals(evaluate(expression, value.get("data")), value.get("expected"));
+      }
+    }
   }
 
   /**
-   * run the test spec in the given file
+   * REST client that calls /rest/expression
    */
-  void testDriver(String file) throws Exception {
-    ObjectMapper om =
-        new ObjectMapper().configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true)
-            .configure(JsonReadFeature.ALLOW_SINGLE_QUOTES.mappedFeature(), true)
-            .configure(JsonReadFeature.ALLOW_JAVA_COMMENTS.mappedFeature(), true);
+  Object evaluate(String expr, Object data) throws Exception {
+    // send to http://admin:djdjdj@localhost:8080
+    URL url = new URL("http://localhost:8080/rest/expression");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("POST");
+    connection.setDoOutput(true);
+    connection.setRequestProperty("content-type", "application/json");
+    connection.setRequestProperty("Authorization",
+        "Basic " + Base64.getEncoder().encodeToString("admin:djdjdj".getBytes()));
+    Map<String, Object> expAndData = new HashMap<>();
+    expAndData.put("data", data);
+    expAndData.put("expression", expr);
 
-    // read file and select expression
-    JsonNode map = om.readTree(new FileInputStream(new File(file)));
-    file = map.get("test").get("file").asText();
-    JsonNode test = om.readTree(new FileInputStream(new File(file)));
-    String expr =
-        Expression.jsonata(map.get("test").get("expression").asText()).evaluate(test).asText();
-
-    // iterate cases
-    Iterator<String> cases = map.get("cases").fieldNames();
-    while (cases.hasNext()) {
-      String name = cases.next();
-      ObjectNode c = (ObjectNode) map.get("cases").get(name);
-
-      if (c.has("url")) {
-        URL url = new URL(c.get("url").asText());
-        if (c.get("url").asText().endsWith(".json"))
-          c.set("data", objectMapper.readTree(url));
-        else
-          c.put("data", IOUtils.toString(url.openStream()));
-      }
-
-      if (map.has("basedata")) {
-        ObjectNode basedata = map.get("basedata").deepCopy();
-
-        // merge base data
-        Iterator<String> dataFields = c.get("data").fieldNames();
-        while (dataFields.hasNext()) {
-          String field = dataFields.next();
-          basedata.set(field, c.get("data").get(field));
-        }
-
-        // eval and check
-        Assert.assertEquals("error in case: " + name, "" + c.get("expected"),
-            "" + evaluate(expr, basedata));
-      } else {
-        // eval and check
-        Assert.assertEquals("error in case: " + name, "" + c.get("expected"),
-            "" + evaluate(expr, c.get("data")));
-      }
-    }
+    IOUtils.write(new ObjectMapper().writeValueAsString(expAndData), connection.getOutputStream(),
+        Charset.defaultCharset());
+    return Json.parseJson(new InputStreamReader(connection.getInputStream()));
   }
 
-  @SuppressWarnings("unused")
-  JsonNode evaluate(String expr, JsonNode basedata) throws Exception {
-    if (false)
-      // uses local JSONata4J (not recommended since there are slight differences to JSONataJS)
-      return Expression.jsonata(expr).evaluate(basedata);
-    else {
-      // send to http://admin:djdjdj@localhost:8080
-      URL url = new URL("http://localhost:8080/rest/expression");
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setDoOutput(true);
-      connection.setRequestProperty("content-type", "application/json");
-      connection.setRequestProperty("Authorization",
-          "Basic " + Base64.getEncoder().encodeToString("admin:djdjdj".getBytes()));
-      ObjectNode expAndData = objectMapper.createObjectNode();
-      expAndData.set("data", basedata);
-      expAndData.put("expression", expr);
-      objectMapper.writeValue(new OutputStreamWriter(connection.getOutputStream()), expAndData);
-      return objectMapper.readTree(connection.getInputStream());
-    }
-  }
-
-  @SuppressWarnings("unused")
-  void parse(String expression) throws Exception {
-    if (false) {
-      // uses local JSONata4J (not recommended since there are slight differences to JSONataJS)
-      Expressions expr = Expressions.parse(expression);
-
-      // register Dashjoin custom functions
-      for (String function : functions)
-        expr.getEnvironment().setJsonataFunction(function, new FloorFunction());
-      try {
-        // evaluate
-        expr.evaluate(null);
-      } catch (EvaluateException e) {
-        // function signature does not match is expected
-        Assert.assertTrue("Invalid expression: " + expression,
-            e.getMessage().contains("does not match function signature"));
-      }
-    } else {
-      // send to http://admin:djdjdj@localhost:8080
-      URL url = new URL("http://localhost:8080/rest/expression-preview/parse");
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setDoOutput(true);
-      connection.setRequestProperty("content-type", "application/json");
-      connection.setRequestProperty("Authorization",
-          "Basic " + Base64.getEncoder().encodeToString("admin:djdjdj".getBytes()));
-      try (OutputStream os = connection.getOutputStream()) {
-        os.write(expression.getBytes());
-      }
-      objectMapper.readTree(connection.getInputStream());
-    }
+  /**
+   * converts null, string, [string] to list of strings
+   */
+  List<String> strings(Object arg) {
+    if (arg == null)
+      return Arrays.asList();
+    if (arg instanceof List)
+      return (List<String>) arg;
+    return Arrays.asList((String) arg);
   }
 }
